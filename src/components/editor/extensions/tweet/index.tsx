294f15e9c7ccf99dbca42905931cf3d0650e655e
yuuki008 <s22ah261@gmail.com>
2025-03-29 21:51:51 +0900

    Update tweet component and global CSS variables- Update tweet component to use a separate file- Update global CSS variables for popover and primary colors

--- src/components/editor/extensions/tweet/index.tsx
+++ src/components/editor/extensions/tweet/index.tsx
@@ -1,19 +1,6 @@
-import { NodeViewProps, NodeViewWrapper } from "@tiptap/react";
 import { nodePasteRule, ReactNodeViewRenderer } from "@tiptap/react";
 import { mergeAttributes, Node } from "@tiptap/core";
-import { Tweet } from "react-tweet";
-
-export const TweetComponent = ({ node }: NodeViewProps) => {
-  const url = node.attrs.url;
-  const tweetIdRegex = /\/status\/(\d+)/g;
-  const id = tweetIdRegex.exec(url)?.[1];
-
-  return (
-    <NodeViewWrapper className="max-w-[500px] w-full mx-auto">
-      <Tweet id={id || ""} />
-    </NodeViewWrapper>
-  );
-};
+import TweetComponent from "./tweet-component";
 
 export const TweetExtension = Node.create({
   name: "twitter",
@@ -59,11 +46,6 @@ export const TweetExtension = Node.create({
   renderHTML({ HTMLAttributes }) {
     return ["twitter", mergeAttributes(HTMLAttributes)];
   },
-
-  // TODO: WIP
-  // addCommands() {
-  //   return {}
-  // },
 
   addNodeView() {
     return ReactNodeViewRenderer(TweetComponent);
---
+++ src/components/editor/extensions/tweet/tweet-component.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { TweetProps, useTweet } from "react-tweet";
+
+import {
+  MagicTweet,
+  TweetNotFound,
+  TweetSkeleton,
+} from "@/components/ui/tweet";
+import { NodeViewProps } from "@tiptap/react";
+import { NodeViewWrapper } from "@tiptap/react";
+
+function ClientTweetCard({
+  id,
+  apiUrl,
+  fallback = <TweetSkeleton />,
+  components,
+  fetchOptions,
+  onError,
+  ...props
+}: TweetProps & { className?: string }) {
+  const { data, error, isLoading } = useTweet(id, apiUrl, fetchOptions);
+
+  if (isLoading) return fallback;
+  if (error || !data) {
+    const NotFound = components?.TweetNotFound || TweetNotFound;
+    return <NotFound error={onError ? onError(error) : error} />;
+  }
+
+  return <MagicTweet tweet={data} components={components} {...props} />;
+}
+
+export default function TweetComponent({ node }: NodeViewProps) {
+  const url = node.attrs.url;
+  const tweetIdRegex = /\/status\/(\d+)/g;
+  const id = tweetIdRegex.exec(url)?.[1];
+
+  return (
+    <NodeViewWrapper className="max-w-[500px] w-full mx-auto my-4">
+      <ClientTweetCard id={id || ""} />
+    </NodeViewWrapper>
+  );
+}
