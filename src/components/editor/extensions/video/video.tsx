547c824f7bbe89a918037709cbd78360ad51990d
yuuki008 <s22ah261@gmail.com>
2025-03-30 11:22:19 +0900

    Refactor video component loading skeleton styles

--- src/components/editor/extensions/video/video.tsx
+++ src/components/editor/extensions/video/video.tsx
@@ -7,14 +7,14 @@ export function VideoComponent({ node }: NodeViewProps) {
   if (loading) {
     return (
       <NodeViewWrapper>
-        <Skeleton className="w-full h-[450px] rounded-lg" />
+        <Skeleton className="w-full h-[450px] rounded-lg my-4" />
       </NodeViewWrapper>
     );
   }
 
   return (
     <NodeViewWrapper>
-      <video controls className="w-full rounded-lg">
+      <video controls className="w-full rounded-lg my-4">
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
