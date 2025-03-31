45582a529162e1ce76fb2db2ace8da923dd555c2
yuuki008 <s22ah261@gmail.com>
2025-03-31 19:30:01 +0900

    Refactor EmojiExtension import path in editor/extensions/index.ts

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -23,7 +23,7 @@ import { OGPLink } from "./open-graph";
 import { Image } from "./image";
 import { Video } from "./video";
 import { YouTubeExtension } from "./youtube";
-import EmojiExtension from "./emoji/index";
+import EmojiExtension from "@/components/editor/extensions/emoji";
 import FileHandlerExtension from "./file-handler";
 
 export const extensions = [
