12704153207d306c7b2e82ad0f81558899291ed6
yuuki008 <s22ah261@gmail.com>
2025-03-31 19:36:53 +0900

    Fix import statement casing for Search component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Search from "./components/search";
+import Search from "./components/Search";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { AuthProvider } from "@/components/providers/auth-provider";
 import Header from "./components/header";
