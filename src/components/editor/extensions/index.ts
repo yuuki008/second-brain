4488493193dce0f80e49bad5d7871ec6ba65405c
yuuki008 <s22ah261@gmail.com>
2025-03-31 19:28:03 +0900

    Update import path for EmojiExtension in editor extensions index.ts file

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -23,7 +23,7 @@ import { OGPLink } from "./open-graph";
 import { Image } from "./image";
 import { Video } from "./video";
 import { YouTubeExtension } from "./youtube";
-import EmojiExtension from "./emoji";
+import EmojiExtension from "./emoji/index";
 import FileHandlerExtension from "./file-handler";
 
 export const extensions = [
