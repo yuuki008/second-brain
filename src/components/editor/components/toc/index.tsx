033d094e78ab6249a7f2d9e5a2b5e0716bec569a
yuuki008 <s22ah261@gmail.com>
2025-04-02 09:55:46 +0900

    Add Table of Contents (ToC) component to the editor

---
+++ src/components/editor/components/toc/index.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+import {
+  TableOfContentData,
+  TableOfContentDataItem,
+} from "@tiptap-pro/extension-table-of-contents";
+import { TextSelection } from "@tiptap/pm/state";
+import { Editor } from "@tiptap/react";
+
+type ToCItemProps = {
+  item: TableOfContentDataItem;
+  onItemClick: (e: React.MouseEvent<HTMLAnchorElement>, id: string) => void;
+};
+
+export const ToCItem = ({ item, onItemClick }: ToCItemProps) => {
+  return (
+    <div
+      className={cn(item.isActive ? "text-primary" : "text-secondary")}
+      style={{
+        paddingLeft: `${item.level * 10}px`,
+      }}
+    >
+      <a
+        href={`#${item.id}`}
+        onClick={(e) => onItemClick(e, item.id)}
+        data-item-index={item.itemIndex}
+      >
+        {item.textContent}
+      </a>
+    </div>
+  );
+};
+
+type Props = {
+  items: TableOfContentData;
+  editor: Editor;
+};
+
+export default function ToC({ items, editor }: Props) {
+  if (!items || items.length === 0) return null;
+
+  const onItemClick = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
+    e.preventDefault();
+    if (!editor) return;
+
+    const element = editor.view.dom.querySelector(`[data-toc-id="${id}"`);
+    if (!element) return;
+
+    const pos = editor.view.posAtDOM(element, 0);
+
+    // set focus
+    const tr = editor.view.state.tr;
+    tr.setSelection(new TextSelection(tr.doc.resolve(pos)));
+    editor.view.dispatch(tr);
+    editor.view.focus();
+
+    history.pushState(null, "", `#${id}`);
+
+    window.scrollTo({
+      top: element.getBoundingClientRect().top + window.scrollY,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="fixed bottom-4 right-4 z-50 bg-background/80 backdrop-blur-sm p-4 rounded-lg shadow-lg">
+      <ul className="space-y-1">
+        {items.map((item) => (
+          <ToCItem onItemClick={onItemClick} key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,10 +1,11 @@
-import { useEditor, EditorContent, AnyExtension } from "@tiptap/react";
+import { useEditor, EditorContent } from "@tiptap/react";
 import { generateExtensions } from "./extensions";
 import { cn } from "@/lib/utils";
 import { TextMenu } from "./components/text-menu";
 import "./styles/markdown.css";
 import { TableOfContentData } from "@tiptap-pro/extension-table-of-contents";
 import { useState } from "react";
+import ToC from "./components/toc";
 
 type Props = {
   content: string;
@@ -37,11 +38,14 @@ const Editor = ({ content, onChange, className, readOnly = false }: Props) => {
 
   if (!editor) return <></>;
 
-  console.log(tableOfContentData);
+  // NOTE: tiptap の初期化が完了せずに ToC コンポーネントを呼び出すとエラーが出るので、
+  // 初期化が完了してから ToC コンポーネントを呼び出すようにする
+  if (!editor.isInitialized) return <></>;
 
   return (
-    <div className={cn("markdown-editor", className)}>
+    <div className={cn("markdown-editor relative", className)}>
       <EditorContent className="h-full" editor={editor} />
+      <ToC items={tableOfContentData} editor={editor} />
       {!readOnly && <TextMenu editor={editor} />}
     </div>
   );
