33c85231036989b57a86b6c18e7ed2b26edca7ea
yuuki008 <s22ah261@gmail.com>
2025-04-17 20:49:45 +0900

    Add Zen mode functionality to the Header component

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,12 +3,20 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { useZen } from "@/components/providers/zen-provider";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
   const { theme } = useTheme();
+  const { isZenMode } = useZen();
 
   return (
-    <div className="fixed top-0 left-0 pl-5 z-[30] bg-transparent py-4">
+    <div
+      className={cn(
+        "fixed top-0 left-0 pl-5 z-[30] bg-transparent py-4 transition-all duration-300",
+        isZenMode && "hidden"
+      )}
+    >
       <Link className="cursor-pointer" href="/">
         <Image
           src={theme === "dark" ? "/dark-sign.png" : "/light-sign.png"}
